302e8fa3c8a62fe95cb73f9dca695da64ce73693
Andrew Schaper <andrew@schaper.io>
2018-07-03 20:56:16 -0700

    Added more Container tests

--- hera/container_test.go
+++ hera/container_test.go
@@ -1,24 +1,96 @@
 package main
 
-// func TestVerifyLabels(t *testing.T) {
-// 	labels := map[string]string{}
-// 	c := &Container{
-// 		ID:       "2134",
-// 		Hostname: "host.name",
-// 		Labels:   labels,
-// 	}
+import (
+	"testing"
 
-// 	if err := c.VerifyLabels(); err == nil {
-// 		t.Error("want error for no labels")
-// 	}
+	"github.com/spf13/afero"
+)
 
-// 	labels["hera.hostname"] = "address"
-// 	if err := c.VerifyLabels(); err == nil {
-// 		t.Error("want error for one hera label")
-// 	}
+var container = &Container{
+	ID:       "f5020368c5bf",
+	Hostname: "hostname",
+	Labels:   map[string]string{},
+}
 
-// 	labels["hera.port"] = "80"
-// 	if err := c.VerifyLabels(); err != nil {
-// 		t.Error(err)
-// 	}
-// }
+func TestGetHostname(t *testing.T) {
+	_, err := container.getHostname()
+	if err == nil {
+		t.Error("want error")
+	}
+
+	expected := "a.host.com"
+	container.Labels = map[string]string{
+		"hera.hostname": expected,
+	}
+
+	hostname, err := container.getHostname()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if hostname != expected {
+		t.Errorf("Unexpected hostname: %s", hostname)
+	}
+}
+
+func TestGetPort(t *testing.T) {
+	_, err := container.getPort()
+	if err == nil {
+		t.Error("want error")
+	}
+
+	expected := "8080"
+	container.Labels = map[string]string{
+		"hera.port": expected,
+	}
+
+	port, err := container.getPort()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if port != expected {
+		t.Errorf("Unexpected port: %s", port)
+	}
+}
+
+func TestGetCertificateUsesDefaultCert(t *testing.T) {
+	cert, err := container.getCertificate()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if cert == nil {
+		t.Error("Expected certificate")
+	}
+}
+
+func TestGetCertificateNonexistent(t *testing.T) {
+	certname := "mysite.com.pem"
+	container.Labels = map[string]string{
+		"hera.certificate": certname,
+	}
+
+	_, err := container.getCertificate()
+	if err == nil {
+		t.Error("want error")
+	}
+}
+
+func TestGetCertificateExistingCustom(t *testing.T) {
+	certname := "mysite.com.pem"
+	cert := NewCertificate(certname)
+	err := afero.WriteFile(fs, cert.fullPath(), []byte(""), 0644)
+	if err != nil {
+		t.Error(err)
+	}
+
+	foundcert, err := container.getCertificate()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if foundcert.Name != certname {
+		t.Errorf("Unexpected certificate name: %s", foundcert.Name)
+	}
+}
